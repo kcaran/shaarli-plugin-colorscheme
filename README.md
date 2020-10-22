0f74078937a1be67fb70da4d9363fb6a0e145f55
Keith Carangelo <mail@kcaran.com>
2020-10-22 10:45:30 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# shaarli-plugin-colorscheme
-Colorscheme is a Shaarli plugin that displays a color in #xxyyzz format as an inline sample block.
+# Shaarli Colorscheme plugin
+
+**Colorscheme** is a plugin for [Shaarli](https://github.com/shaarli/shaarli)
+that displays a color in #xxyyzz format as an inline sample block.
+
+Example with Shaarli Shirley theme:
+
+![Shaarli Colorscheme plugin preview](https://raw.githubusercontent.com/kcaran/shaarli-plugin-colorscheme/master/preview.png)
+
+## Installation
+### Via Git
+
+Run the following command from the plugins folder
+of your Shaarli installation:
+
+```sh
+git clone https://github.com/kcaran/shaarli-plugin-colorscheme colorscheme
+```
+
+It'll create the `colorscheme` folder.
+
+### Manually
+
+Create the folder plugins/colorscheme in your Shaarli installation. Download the ZIP file of this repository and copy all files in the newly created folder.
+
+## Activation
+If your Shaarli installation is recent enough to have the plugin administration page, you just need to go to the plugin administration page, check `colorscheme` and save.
+
+## Configure
+TBD: Add the ability to modify the default styles.
+
+TBD: The current plugin overrides the add-tag URL. I'm not sure if that
+is the desired effect.
+
+## Update
+I you installed it through Git, run the following command from within this plugin's folder `plugins/colorscheme`:
+
+```shell
+git pull
+```
+
+Otherwise, download the ZIP file again from Github and override existing files with new ones.
---
+++ colorscheme.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Plugin Colorscheme
+ *
+ * Displays a color in #xxyyzz format as an inline sample block.
+ */
+
+use Shaarli\Plugin\PluginManager;
+use Shaarli\Render\TemplatePage;
+
+/* HSP Color Model - http://alienryderflex.com/hsp.html */
+function dark_color( $match ) {
+  $brightness = sqrt( .299 * pow( hexdec( $match[1] ), 2 ) + .587 * pow( hexdec( $match[2] ), 2 ) + .114 * pow( hexdec( $match[3] ), 2 ) );
+  return ($brightness <= 127.5);
+}
+
+function display_color( $match ) {
+  $hex_color = '#' . strtolower( $match[1] ) . strtolower( $match[2] ) . strtolower( $match[3] );
+
+  $dark = dark_color( $match ) ? ' dark' : '';
+
+  return '<span class="colorscheme' . $dark . '" style="background-color: ' .  $hex_color . '">' . $hex_color .  '</span>';
+}
+
+function colorscheme_url( $url ) {
+  $pattern = '/^https:\/\/assets.adobe.com\//i';
+  return preg_match( $pattern, $url ) ? 1 : 0;
+}
+
+function hook_colorscheme_render_editlink( $data ) {
+  if ($data['link_is_new'] && colorscheme_url( $data['link']['url'] )) {
+    $data['link']['tags'] = 'colorscheme';
+   }
+
+  return $data;
+}
+
+function hook_colorscheme_render_linklist( $data ) {
+    #$pattern = '/<a href="[^"]+">(#[a-f0-9]{6})</a>/i';
+    $pattern = '/<a href="[^"]+">#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})<\/a>(?:\r\n|\n)?(?:<br\s*\/?>)?/msi';
+    foreach ($data['links'] as &$value) {
+      $value['description'] = preg_replace_callback( $pattern,
+		"display_color",
+		$value['description'], -1, $count );
+    }
+
+  return $data;
+}
+
+function hook_colorscheme_render_includes( $data ) {
+  if ($data['_PAGE_'] == TemplatePage::LINKLIST) {
+    $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/colorscheme/colorscheme.css';
+  }
+
+  return $data;
+}
+
+/**
+ * This function is never called, but contains translation calls for GNU gettext extraction.
+ */
+function colorscheme_dummy_translation()
+{
+    // meta
+    t('Displays a color in #xxyyzz format as an inline sample block.');
+}
