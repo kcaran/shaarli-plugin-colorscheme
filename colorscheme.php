8474ab0af78628cdb64bb9a5de384fe9fa89c86b
Keith Carangelo <mail@kcaran.com>
2024-05-14 11:39:16 -0400

    Ran code through phpcs for formatting issues

--- colorscheme.php
+++ colorscheme.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Colorscheme
  *
@@ -9,50 +10,64 @@ use Shaarli\Plugin\PluginManager;
 use Shaarli\Render\TemplatePage;
 
 /* HSP Color Model - http://alienryderflex.com/hsp.html */
-function dark_color( $match ) {
-  $brightness = sqrt( .299 * pow( hexdec( $match[1] ), 2 ) + .587 * pow( hexdec( $match[2] ), 2 ) + .114 * pow( hexdec( $match[3] ), 2 ) );
-  return ($brightness <= 127.5);
+function dark_color($match)
+{
+    $brightness = sqrt(.299 * pow(hexdec($match[1]), 2)
+        + .587 * pow(hexdec($match[2]), 2)
+        + .114 * pow(hexdec($match[3]), 2));
+    return ($brightness <= 127.5);
 }
 
-function display_color( $match ) {
-  $hex_color = '#' . strtolower( $match[1] ) . strtolower( $match[2] ) . strtolower( $match[3] );
+function display_color($match)
+{
+    $hex_color = '#' . strtolower($match[1]) . strtolower($match[2]) . strtolower($match[3]);
 
-  $dark = dark_color( $match ) ? ' dark' : '';
+    $dark = dark_color($match) ? ' dark' : '';
 
-  return '<a class="colorscheme' . $dark . '" style="background-color: ' .  $hex_color . '" href="#">' . $hex_color .  '</a>';
+    return '<a class="colorscheme' . $dark
+        . '" style="background-color: ' .  $hex_color
+        . '" href="#">' . $hex_color .  '</a>';
 }
 
-function colorscheme_url( $url ) {
-  $pattern = '/^https:\/\/assets.adobe.com\//i';
-  return preg_match( $pattern, $url ) ? 1 : 0;
+function colorscheme_url($url)
+{
+    $pattern = '/^https:\/\/assets.adobe.com\//i';
+    return preg_match($pattern, $url) ? 1 : 0;
 }
 
-function hook_colorscheme_render_editlink( $data ) {
-  if ($data['link_is_new'] && colorscheme_url( $data['link']['url'] )) {
-    $data['link']['tags'] = 'colorscheme';
-   }
+function hook_colorscheme_render_editlink($data)
+{
+    if ($data['link_is_new'] && colorscheme_url($data['link']['url'])) {
+        $data['link']['tags'] = 'colorscheme';
+    }
 
-  return $data;
+    return $data;
 }
 
-function hook_colorscheme_render_linklist( $data ) {
+function hook_colorscheme_render_linklist($data)
+{
     #$pattern = '/<a href="[^"]+">(#[a-f0-9]{6})</a>/i';
     $pattern = '/<a href="[^"]+">#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})<\/a>(?:\r\n|\n)?(?:<br\s*\/?>)?/msi';
     foreach ($data['links'] as &$value) {
-      $value['description'] = preg_replace_callback( $pattern,
-		"display_color",
-		$value['description'], -1, $count );
+        $value['description'] = preg_replace_callback(
+            $pattern,
+            "display_color",
+            $value['description'],
+            -1,
+            $count
+        );
     }
 
-  return $data;
+    return $data;
 }
 
-function hook_colorscheme_render_includes( $data ) {
-  if ($data['_PAGE_'] == TemplatePage::LINKLIST) {
-    $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/colorscheme/colorscheme.css';
-  }
+function hook_colorscheme_render_includes($data)
+{
+    if ($data['_PAGE_'] == TemplatePage::LINKLIST) {
+        $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/colorscheme/colorscheme.css';
+    }
 
-  return $data;
+    return $data;
 }
 
 function hook_colorscheme_render_footer($data)
